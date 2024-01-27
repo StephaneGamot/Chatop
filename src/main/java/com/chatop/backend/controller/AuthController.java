db4645cae0c2a3878d8bd8e9e8577987b25d5e95
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-01-27 15:49:34 +0100

    GET /me

--- src/main/java/com/chatop/backend/controller/AuthController.java
+++ src/main/java/com/chatop/backend/controller/AuthController.java
@@ -8,14 +8,11 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.chatop.backend.service.JWTService;
 
 @RestController
-// @RequestMapping("/api/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     private final JWTService jwtService;
@@ -65,5 +62,22 @@ public class AuthController {
         } catch (Exception ex) {                                          // Gère toute autre exception inattendue
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erreur lors de l'enregistrement");
         }
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> me(Authentication authentication) {
+        if (authentication == null || !authentication.isAuthenticated()) { // Si l'utilisateur est authentifié
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Utilisateur non authentifié");
+        }
+
+        String username = authentication.getName();                        // Récupérer le nom d'utilisateur depuis l'objet Authentication
+
+        User user = userRepository.findByName(username);                   // Rechercher l'utilisateur dans la base de données
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Utilisateur non trouvé");
+        }
+
+        User userInfo = new User(user.getId(), user.getEmail(), user.getName(), null, user.getCreatedAt(), user.getUpdatedAt());
+        return ResponseEntity.ok(userInfo);                                // Retourner les informations de l'utilisateur (à l'exception du mot de passe)
     }
 }
